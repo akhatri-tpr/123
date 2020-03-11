ad5604c82bc4b8b8f8413389197abdb94112e8e3
akhatri-tps <54460309+khatriajay18@users.noreply.github.com>
2020-03-11 16:56:19 -0400

    4

--- project/A/a.tf
+++ project/A/a.tf
@@ -6,8 +6,5 @@ provider "aws" {
 module "a" {
     source = "../../module"
 }
-<<<<<<< HEAD
 ok
-=======
 done
->>>>>>> 64aa939e2f877e97f2d944861589c2a39695b2fe
--- project/B/b.tf
+++ project/B/b.tf
@@ -6,8 +6,5 @@ provider "aws" {
 module "a" {
     source = "../../module"
 }
-<<<<<<< HEAD
 fine
-=======
 complete
->>>>>>> 64aa939e2f877e97f2d944861589c2a39695b2fe
